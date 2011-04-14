b98168229a4df3dac58b0b4d6f1b6915263fc33a
d.dentsas <d.dentsas@googlemail.com>
2011-04-14 11:34:00 +0000

    Added new WS Interface methods

--- FragmentoServiceClient/src/fragmentserviceTest/FragmentoAxisTest.java
+++ FragmentoServiceClient/src/fragmentserviceTest/FragmentoAxisTest.java
@@ -1,5 +1,6 @@
 package fragmentserviceTest;
 
+import org.apache.axiom.om.impl.llom.util.AXIOMUtil;
 import eu.compas_ict.www.fragmentservice.FragmentServiceStub;
 
 /**
@@ -28,11 +29,10 @@ public class FragmentoAxisTest {
 	 */
 	public static void main(String[] args) {
 		try {
-
-			//System.out.println(retrieveArtefact(3l).getArtefact().getDescription());
-			// System.out.println(retrieveArtefact(3l).getArtefactId());
-			System.out.println(browseArtefacts("WSDL").getArtefactDescriptors().getArtefact().length);
-
+			//System.out.println(retrieveArtifact(3l).getArtefact().getDescription());
+			// System.out.println(retrieveArtifact(3l).getArtefactId());
+			//System.out.println(browseArtifact("WSDL").getArtefactDescriptors().getArtefact().length);
+			//createArtifact("WSDL", "This is a test", "<echo xmlns='asdf'><a>Hi! This is a sample Request.</a></echo>");
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println("\n\n\n");
@@ -42,20 +42,20 @@ public class FragmentoAxisTest {
 
 	/**
 	 * 
-	 * This method retrieves an artefact via its artefactId and returns a
+	 * This method retrieves an artifact via its artifactId and returns a
 	 * RetrieveArtefact response message.
 	 * 
-	 * @param artefactId
-	 *            The artefact request Id in long format
+	 * @param artifactId
+	 *            The artifact request Id in long format
 	 * @return Returns a RetrieveArtefactRepsonseMessage or null if exception is
 	 *         caught
 	 */
-	public static FragmentServiceStub.RetrieveArtefactResponseMessage retrieveArtefact(
-			long artefactId) {
+	public static FragmentServiceStub.RetrieveArtefactResponseMessage retrieveArtifact(
+			long artifactId) {
 		try {
 			FragmentServiceStub.RetrieveArtefactRequestMessage req = new FragmentServiceStub.RetrieveArtefactRequestMessage();
 			FragmentServiceStub.ArtefactSelectorType type = new FragmentServiceStub.ArtefactSelectorType();
-			type.setArtefactId(artefactId);
+			type.setArtefactId(artifactId);
 			req.setArtefactSelector(type);
 
 			return new FragmentServiceStub(serviceURI).retrieveArtefact(req);
@@ -67,18 +67,18 @@ public class FragmentoAxisTest {
 	}
 
 	/**
-	 * This method retrieves all artefacts of a specific type
+	 * This method retrieves all artifacts of a specific type
 	 * 
-	 * @param artefactType
-	 *            Type of artefact e.g. WSDL, Process
+	 * @param artifactType
+	 *            Type of artifact e.g. WSDL, Process
 	 * @return Returns the corresponding broseArtefactsResponse
 	 */
-	public static FragmentServiceStub.BrowseArtefactsResponseMessage browseArtefacts(
-			String artefactType) {
+	public static FragmentServiceStub.BrowseArtefactsResponseMessage browseArtifact(
+			String artifactType) {
 		try {
 			FragmentServiceStub.BrowseArtefactsRequestMessage req = new FragmentServiceStub.BrowseArtefactsRequestMessage();
 			FragmentServiceStub.BrowseArtefactSelectorType type = new FragmentServiceStub.BrowseArtefactSelectorType();
-			type.setType(artefactType);
+			type.setType(artifactType);
 			req.setBrowseArtefactSelector(type);
 
 			return new FragmentServiceStub(serviceURI).browseArtefacts(req);
@@ -89,5 +89,29 @@ public class FragmentoAxisTest {
 		}
 		return null;
 	}
-
+	
+	/**
+	 * This method creates a new artifact.
+	 * 
+	 * @param artifactType Type of artifact e.g. WSDL, Process
+	 * @param description Description of the artifact
+	 * @param payload
+	 * @return createArtefactResponseMessage
+	 */
+	public static FragmentServiceStub.CreateArtefactResponseMessage createArtifact(String artifactType, String description, String payload) {
+		try {			
+			FragmentServiceStub.CreateArtefactRequestMessage req = new FragmentServiceStub.CreateArtefactRequestMessage();			
+			FragmentServiceStub.ArtefactType type = new FragmentServiceStub.ArtefactType();
+			type.setType(artifactType);
+			req.setArtefact(type);
+			req.getArtefact().setDescription(description);
+			req.getArtefact().setExtraElement(AXIOMUtil.stringToOM(payload));
+			
+			return new FragmentServiceStub(serviceURI).createArtefact(req);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("\n\n\n");
+		}
+		return null;
+	}
 }
